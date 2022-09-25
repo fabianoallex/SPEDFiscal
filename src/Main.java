82da65974deb15bcae8f9703db90df161471678e
Fabiano Arndt <fabianoallex@gmail.com>
2022-09-25 10:16:55 -0400

    removção de imports não utilizados

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,4 @@
 import java.io.FileWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
