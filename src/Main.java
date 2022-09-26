ceda31f9fe8ac40d5a0452a38ff0e32b40409b01
Fabiano Arndt <fabianoallex@gmail.com>
2022-09-26 05:11:55 -0400

    criação de duas classes que implementam a interface Writer: SPEDFileWriter e SPEDStringBuilder.

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,6 @@
 import java.io.FileWriter;
 import java.io.PrintWriter;
 
-class SPEDFileWriter implements Writer {
-    private final PrintWriter printWriter;
-
-    public SPEDFileWriter(FileWriter fileWriter) {
-        this.printWriter = new PrintWriter(fileWriter);
-    }
-
-    @Override
-    public void write(String string) {
-        this.printWriter.println(string);
-    }
-}
-
-class SPEDStringBuilder implements Writer {
-    private final StringBuilder stringBuilder;
-    public SPEDStringBuilder(StringBuilder stringBuilder) {
-        this.stringBuilder = stringBuilder;
-    }
-
-    @Override
-    public void write(String string) {
-        this.stringBuilder.append(string);
-        this.stringBuilder.append("\n\r");
-    }
-
-    public StringBuilder getStringBuilder() {
-        return stringBuilder;
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
         try {
--- src/Writer.java
+++ src/Writer.java
@@ -1,3 +1,36 @@
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
 public interface Writer {
     void write(String string);
+}
+
+class SPEDFileWriter implements Writer {
+    private final PrintWriter printWriter;
+
+    public SPEDFileWriter(FileWriter fileWriter) {
+        this.printWriter = new PrintWriter(fileWriter);
+    }
+
+    @Override
+    public void write(String string) {
+        this.printWriter.println(string);
+    }
+}
+
+class SPEDStringBuilder implements Writer {
+    private final StringBuilder stringBuilder;
+    public SPEDStringBuilder(StringBuilder stringBuilder) {
+        this.stringBuilder = stringBuilder;
+    }
+
+    @Override
+    public void write(String string) {
+        this.stringBuilder.append(string);
+        this.stringBuilder.append("\n\r");
+    }
+
+    public StringBuilder getStringBuilder() {
+        return stringBuilder;
+    }
 }
