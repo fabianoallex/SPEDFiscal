1f588e305146717dbb1d42a4b79b71065c960d1c
Fabiano Arndt <fabianoallex@gmail.com>
2022-12-22 21:56:14 -0400

    gerado testes para método generateBlock9() da classe SpedGenerator

--- test/java/SpedGeneratorTest.java
+++ test/java/SpedGeneratorTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import sped.core.SpedGenerator;
@@ -98,4 +99,63 @@ public class SpedGeneratorTest {
         */
     }
 
+    @Test @DisplayName("Geração do bloco 9")
+    void generateBlock9Test() {
+        var spedGenerator = SpedGenerator.newBuilder("definitions.xml")
+                .setFileLoader(fileName -> Objects.requireNonNull(SpedGeneratorTest.class.getResourceAsStream(fileName)))
+                .build();
+
+        //#1 - antes de gerar o bloco 9, uma tentantiva de acesso ao bloco 9 deve retornar null
+        var block9 = spedGenerator.getBlock("9");
+        assertNull(block9, "Era esperado que getBlock(\"9\") retornasse null antes de chamar generateBlock9()");
+
+        //#2 - após gerar o bloco 9, deve ser retornado um objeto ao acessar o bloco 9
+        spedGenerator.generateBlock9();
+
+        block9 = spedGenerator.getBlock("9");
+        assertNotNull(block9, "É esperado que após chamar generateBlock9(), seja retornado um objeto com getBlock(\"9\")");
+
+        //#3 - verifica se o bloco possui 7 registros:
+        /*
+        |9001||         //abertura do bloco 9
+        |9900|9001|1|   //totalizacao de registros 9001
+        |9900|0000|1|   //totalizacao de registros 0000
+        |9900|9999|1|   //totalizacao de registros 9999
+        |9900|9990|1|   //totalizacao de registros 9990
+        |9900|9900|5|   //totalizacao de registros 9900
+        |9990||         //encerramento do bloco 9
+        * */
+        assertEquals(7, block9.count(), "#3 - o bloco 9 deveria retornar 7 ao chamar count()");
+
+        //#4 - adiciona um novo bloco e checa se a contagem estará correta
+        /*
+        *
+        * */
+        var blockC = spedGenerator.newBlockBuilder()
+                .setBlockName("C")
+                .setOpeningRegisterName("C001")
+                .setClosureRegisterName("C990")
+                .build();
+
+        spedGenerator.generateBlock9();                 //ao gerar o bloco 9 novamente, deve-se chamar
+        block9 = spedGenerator.getBlock("9"); //getBlock9 novamente para retornar a instancia atual
+
+        assertEquals(9, block9.count(), "#4 - o bloco 9 deveria retornar 9 ao chamar count()");
+
+        //#5 — adicionar um registro ao bloco 9 e testar a contagem do bloco 9
+        var rC100 = blockC.addRegister("C100");
+
+        spedGenerator.generateBlock9();                 //ao gerar o bloco 9 novamente, deve-se chamar
+        block9 = spedGenerator.getBlock("9"); //getBlock9 novamente para retornar a instancia atual
+
+        assertEquals(10, block9.count(), "#5 - o bloco 9 deveria retornar 10 ao chamar count()");
+
+        //#6 — ao adicionar um registro filho de outro registro, o bloco 9 deve retornar um registro a mais
+        rC100.addRegister("C101");
+
+        spedGenerator.generateBlock9();                 //ao gerar o bloco 9 novamente, deve-se chamar
+        block9 = spedGenerator.getBlock("9"); //getBlock9 novamente para retornar a instancia atual
+
+        assertEquals(11, block9.count(), "#6 - o bloco 9 deveria retornar 11 ao chamar count()");
+    }
 }
